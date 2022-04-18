f98be45d4123806bd444be660827cadf3a3e8f78
ziyacaylan <ziyacaylan@gmail.com>
2022-04-18 11:53:23 +0300

    Ders 24 : Sonu güncelleme

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void BlogyAdd(Blog blog);
+        void BlogUpdate(Blog blog);
+        void BlogDelete(Blog blog);
+        List<Blog> GetList();
+        Blog GetByID(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+
+        public void BlogDelete(Blog blog)
+        {
+            _blogDal.Update(blog);
+        }
+
+        public void BlogUpdate(Blog blog)
+        {
+            _blogDal.Update(blog);
+        }
+
+        public void BlogyAdd(Blog blog)
+        {
+            _blogDal.Insert(blog);
+        }
+
+        public Blog GetByID(int id)
+        {
+            return _blogDal.GetByID(id);
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+    }
+}
