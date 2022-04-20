1ae5a05185b3c70758354ec0ad5be5c45759b195
ziyacaylan <ziyacaylan@gmail.com>
2022-04-20 17:09:03 +0300

    Ders 34: Sonu

---
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        void WriterAdd(Writer writer);
+        void WriterUpdate(Writer writer);
+        void WriterDelete(Writer writer);
+        List<Writer> GetList();
+        Writer GetByID(int id);
+    }
+}
---
+++ CoreMyBlog/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMyBlog.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            writer.WriterAbout = "Deneme Test";
+            writer.WriterStatus = true;
+            wm.WriterAdd(writer);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
